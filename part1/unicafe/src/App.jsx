b613a098feec6f1038e032c9e36424d615d3cd69
Nicolás García <nicolasg.ngl@gmail.com>
2024-11-06 20:15:28 -0500

    finishing excercises 1.7 - 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 
-const Title = ({title}) => {
+const Title = ({ title }) => {
   return <h1>{title}</h1>
 }
 
-const Button =({text, handleClick})=> {
+const Button = ({ text, handleClick }) => {
   return (
-    <button 
-    onClick={handleClick}
-    style={{marginInline:'4px'}}>
+    <button
+      onClick={handleClick}
+      style={{ marginInline: '4px' }}>
       {text}
     </button>
   )
 }
 
-const Statitics = ({text, state}) => {
+const Statitics = ({ text, state }) => {
   return (
-    <div style={{display:'flex', gap:'6px'}}>
+    <div style={{ display: 'flex', gap: '6px' }}>
       <p>{text}</p>
-      <p>{state}</p>
+      
+        
+        <p>{state} {text === 'average' && '%'}</p>
+      
     </div>
   )
 }
@@ -27,15 +30,41 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [score, setScore] = useState([])
+  const [average, setAverage] = useState(0)
+
   const handleGood = () => {
+    setScore(prevScore => {
+      const newScore = prevScore.concat(1)
+      setAverage(newScore.reduce((a, b) => a + b, 0) / newScore.length)
+      return newScore
+    })
+    setTotal(good + neutral + bad + 1)
     setGood(good + 1)
   }
   const handleNeutral = () => {
+    setScore(prevScore => {
+      const newScore = prevScore.concat(0)
+      setAverage(newScore.reduce((a, b) => a + b, 0) / newScore.length)
+      return newScore
+    });
+    setTotal(good + bad + neutral + 1)
     setNeutral(neutral + 1)
   }
+
   const handleBad = () => {
+    setScore(prevScore => {
+      const newScore = prevScore.concat(-1)
+      setAverage(newScore.reduce((a, b) => a + b, 0) / newScore.length)
+      return newScore
+    })
+    setTotal(good + neutral + bad + 1)
     setBad(bad + 1)
   }
+  console.log(average);
+
+
   return (
     <div>
       <Title title={'give feedback'} />
@@ -43,9 +72,20 @@ function App() {
       <Button handleClick={handleNeutral} text='neutral' />
       <Button handleClick={handleBad} text='bad' />
       <Title title={'Satistics'} />
-      <Statitics state={good} text='good' />
-      <Statitics state={neutral} text='neutral' />
-      <Statitics state={bad} text='bad' />
+      {
+
+        total !== 0
+          ?
+          <>
+            <Statitics state={good} text='good' />
+            <Statitics state={neutral} text='neutral' />
+            <Statitics state={bad} text='bad' />
+            <Statitics state={total} text='total' />
+            <Statitics state={average} text='average' />
+          </>
+          :
+          <p>No feedback given</p>
+      }
     </div>
   )
 }
