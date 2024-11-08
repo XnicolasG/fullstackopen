3632296ce3788501178cc2265f28cf9b61357f3e
Nicolás García <nicolasg.ngl@gmail.com>
2024-11-08 16:47:47 -0500

    finish exercise 2.7 and 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,39 @@
 import { useState } from 'react'
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
 
   const handleAdd = (e) => {
     e.preventDefault()
-    const newPerson = {
-      name: newName
+    const findExistingName = persons.find(person => person.name === newName) === undefined
+    if (findExistingName) {
+      
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    } else {
+      alert(`${newName} is already added to phonebook`)
     }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
   };
-  
-  const handleInputChange = (e) => {
+
+  const handleNameChange = (e) => {
     setNewName(e.target.value)
-  }
+  };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
+  };
 
   return (
     <div>
@@ -26,8 +43,16 @@ function App() {
       >
         <div>
           name: <input
-          onChange={handleInputChange}
+          onChange={handleNameChange}
           value={newName}
+          required
+          />
+        </div>
+        <div>
+          number: <input 
+          onChange={handleNumberChange}
+          value={newNumber}
+          required
           />
         </div>
         <div>
@@ -38,7 +63,10 @@ function App() {
       <ul>
         {
           persons.map(person => (
-            <p key={person.name}>{person.name}</p>
+            <li key={person.id} style={{display:'flex', gap:'8px'}}>
+            <p >{person.name}</p>
+            <p>{person.number}</p>
+            </li>
           ))
         }
       </ul>
