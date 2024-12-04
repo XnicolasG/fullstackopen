0c7562515451c49f76180010302e0e0f0675882f
Nicolás García <nicolasg.ngl@gmail.com>
2024-12-04 13:25:43 -0500

    exercises 3.20 (backend) finished

--- part3/phonebook/models/Person.js
+++ part3/phonebook/models/Person.js
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
       required: true,
       minLength: 3
     }, 
-    number: String
+    number: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function(v){
+          return /^\d{2,3}-\d{5,}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number`
+      }
+    }
 })
 
 personSchema.set('toJSON', {
