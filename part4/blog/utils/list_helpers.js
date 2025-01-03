3e217cba559121f33377832dcc38d14b3b82f57f
Nicolás García <nicolasg.ngl@gmail.com>
2025-01-03 09:49:39 -0500

    exercise 4.4 finished

--- part4/blog/utils/list_helpers.js
+++ part4/blog/utils/list_helpers.js
@@ -2,6 +2,15 @@ const dummy = blogs => {
     if (blogs) return 1;
 }
 
+const totalLikes = blogs => {
+    if (blogs.length > 0) {
+        return blogs.reduce((acc, blog) => acc + blog.likes, 0);
+    }else {
+        return 0;
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
