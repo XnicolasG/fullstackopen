7b5c7d3a3b83ac419466525e6b311707dc06d53c
Nicolás García <nicolasg.ngl@gmail.com>
2025-01-22 12:36:23 -0500

    Exercises 4.13, 4.14 finished

--- part4/blog/Controller/blog.js
+++ part4/blog/Controller/blog.js
@@ -10,16 +10,47 @@ blogRoutes.get('/', async (request, response) => {
     }
 });
 
+blogRoutes.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
+})
+
 blogRoutes.post('/', async (request, response) => {
     const { title, author, url, likes } = request.body
     !title || !url && response.status(400).json({error: 'title o url missing '})
     try {
-        const blog = new Blog(request.body);
+        const blog = new Blog({
+            title,
+            author: author || 'Anonymous',
+            url,
+            likes: likes || 0
+        });
         const savedBlog = await blog.save();
         response.status(201).json(savedBlog);
     } catch (error) {
         response.status(500).json({ error: 'Failed to save the blog' });
     }
 });
+
+blogRoutes.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+blogRoutes.put('/:id', async (request, response) => {
+    const { title, author, url, likes } = request.body
+    const blog = {
+        title,
+        author,
+        url,
+        likes
+        }
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true, runValidators: true, context: 'query'})
+        response.json(updatedBlog)
+})
 
 module.exports = blogRoutes
