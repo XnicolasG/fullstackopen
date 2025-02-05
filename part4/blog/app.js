51425e32e2fe0570b777a83c1bd35574884e1921
Nicolás García <nicolasg.ngl@gmail.com>
2025-02-04 21:58:31 -0500

    Exercise 4.18 finished

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,12 +3,14 @@ const app = express()
 const blogRoutes = require('./Controller/blog')
 const cors = require('cors');
 const usersRouter = require('./Controller/user');
-const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware');
+const loginRouter = require('./Controller/login');
 
 app.use(express.json());
 app.use(cors())
 app.use('/api/blog', blogRoutes)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
