dcf9e21699e12357cfb55cd70ddbf775c9e7e37c
Nicolás García <nicolasg.ngl@gmail.com>
2025-05-20 14:43:17 -0500

    finish exercise 5.14

--- part2/Blog/src/components/Blog.jsx
+++ part2/Blog/src/components/Blog.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Blog = ({ blogItem, handleLikes, handleDelete }) => {
-
+    const [visible, setVisible] = useState(false)
 
     return (
         <li
             className='flex flex-col gap-2 hover:text-sky-500 cursor-pointer'
             key={blogItem.id}>
             <h4 data-testid='blog_title'>{blogItem?.title}</h4>
-            <p>Url: {blogItem?.url} </p>
-            <p>
-                Likes: {blogItem.likes}
-                <button
-                    className='mx-2'
-                    onClick={handleLikes}
-                >♥️</button>
-            </p>
             <p data-testid='blog_author'>Author: {blogItem.author ? blogItem.author : 'Annonimus'}</p>
+            <button data-testid='blog_showbutton' className='w-34 hover:text-white transition-all duration-150' onClick={() => setVisible(!visible)} >
+                {!visible ? 'Show details' : 'Hide details'}
+            </button>
+
+            {
+                visible &&
+                <article>
+                    <p data-testid='blog_url'>Url: {blogItem?.url} </p>
+                    <p data-testid='blog_likes'>
+                        Likes: {blogItem.likes}
+                        <button
+                            className='mx-2'
+                            onClick={handleLikes}
+                        >♥️</button>
+                    </p>
+                </article>
+            }
             <button onClick={handleDelete} >Delete</button>
         </li>
     )
--- part2/Blog/src/test/Blog.test.jsx
+++ part2/Blog/src/test/Blog.test.jsx
@@ -1,30 +1,38 @@
 import { render, screen } from "@testing-library/react"
 import { expect } from "vitest"
 import { Blog } from "../components/Blog";
+import userEvent from "@testing-library/user-event";
 
-test('Test show ', async () => {
+test('Test show title and author by default ', () => {
     const blog = {
         title: 'Test Blog',
         author: 'Percy',
         url: 'http://test-url.com',
         likes: 5,
     };
-    console.log(screen.debug());
-
     render(<Blog blogItem={blog} />)
 
-    const urlElement = screen.getByText('Url: http://test-url.com');
-    const likesElement = screen.getByText('Likes: 5');
     const title = screen.getByTestId('blog_title')
     const author = screen.getByTestId('blog_author')
 
-
     expect(title).toHaveTextContent('Test Blog')
     expect(author).toHaveTextContent('Percy')
-
-    expect(urlElement).toBeInTheDocument();
-    expect(likesElement).toBeInTheDocument();
-
-    expect(urlElement.offsetParent).toBeNull();
-    expect(likesElement.offsetParent).toBeNull();
 });
+test('clicking the button shows url and likes details', async()=>{
+    const blog = {
+        title: 'Test Blog',
+        author: 'Percy',
+        url: 'http://test-url.com',
+        likes: 5,
+    };
+    render(<Blog blogItem={blog} />)
+    expect(screen.queryByTestId('blog_url')).toBeNull()
+    expect(screen.queryByTestId('blog_likes')).toBeNull()
+
+    const user = userEvent.setup()
+    const button = screen.getByTestId('blog_showbutton')
+    await user.click(button)
+
+    expect(screen.getByTestId('blog_url')).toBeInTheDocument();
+    expect(screen.getByTestId('blog_likes')).toBeInTheDocument();
+})
