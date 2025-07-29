9a2cb41ddf8f9be0d41f92d5acf1164a780ef686
Nicolás García <nicolasg.ngl@gmail.com>
2025-07-28 19:03:01 -0500

    add login form test, successfull

--- part5/notes/tests/note_app.spec.js
+++ part5/notes/tests/note_app.spec.js
@@ -1,12 +1,23 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
-
+  })
+  test('front page can be opened', async ({ page }) => {
     const locator = await page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
+  })
+
+  test('Login form can be opened', async ({ page }) => {
+    await page.getByRole('button', { name: /log in/i }).click()
+
+    await page.getByTestId('username').fill('percy1')
+     await page.getByTestId('password').fill('P3rcy')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByText('Percy logged-in')).toBeVisible()
   })
 })
