b941e6a47164d61aefb74d3956b68b6100b65ba8
Nicolás García <nicolasg.ngl@gmail.com>
2025-07-30 14:55:13 -0500

    add test controlling DB with one just for testing

--- part5/notes/tests/note_app.spec.js
+++ part5/notes/tests/note_app.spec.js
@@ -2,9 +2,27 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 
 describe('Note app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        username: 'percy1',
+        password: 'P3rcy'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
+
+  test('login fails with wrong password', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByTestId('username').fill('percy1')
+    await page.getByTestId('password').fill('wrong')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Wrong credentials, please try again')).toBeVisible()
+  })
+
   test('front page can be opened', async ({ page }) => {
     const locator = await page.getByText('Notes')
     await expect(locator).toBeVisible()
@@ -15,9 +33,34 @@ describe('Note app', () => {
     await page.getByRole('button', { name: /log in/i }).click()
 
     await page.getByTestId('username').fill('percy1')
-     await page.getByTestId('password').fill('P3rcy')
+    await page.getByTestId('password').fill('P3rcy')
     await page.getByRole('button', { name: 'Login' }).click()
 
-    await expect(page.getByText('Percy logged-in')).toBeVisible()
+    await expect(page.getByText('logged-in')).toBeVisible()
+  })
+
+  describe('when logged a note exist', () => {
+    beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing')
+
+      await page.getByRole('button', { name: /log in/i }).click()
+
+      await page.getByTestId('username').fill('percy1')
+      await page.getByTestId('password').fill('P3rcy')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('logged-in')).toBeVisible()
+
+      await page.getByRole('button', { name: 'New note' }).click()
+      await page.getByTestId('new note').fill('another note by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.locator('li:nth-last-child(1)').getByRole('button', { name: 'make not important' }).click()
+      await expect(
+        page.locator('li:nth-last-child(1)').getByText('make important')
+      ).toBeVisible({ timeout: 30000 })
+    })
   })
 })
